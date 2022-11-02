95e9e9ebdc84c0326c557442a17ce1aec70cbd77
aleksandar.marjanovic <aleksandar.marjanovic@quantoxtechnology.com>
2022-11-02 18:50:52 +0100

    create migrations

---
+++ database/migrations/2022_11_02_120732_create_payments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('type');
+            $table->string('name');
+            $table->date('payment_date');
+            $table->string('interval');
+            $table->integer('amount')->nullable();
+            $table->string('receiver');
+            $table->string('receiver_account_number');
+            $table->string('payment_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+};
---
+++ database/migrations/2022_11_02_122001_create_records_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['income', 'expense', 'transfer']);
+            $table->foreignId('account_id');
+            $table->foreignId('to_account_id')->nullable()->references('id')->on('accounts');
+            $table->foreignId('subcategory_id');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+};
