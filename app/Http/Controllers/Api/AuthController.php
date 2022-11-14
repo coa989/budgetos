1e5a2905e5d22393d82304bc911232caac9d80e2
aleksandar.marjanovic <aleksandar.marjanovic@quantoxtechnology.com>
2022-11-14 18:02:51 +0100

    install laravel swagger, add and generate docs for auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Actions\CreateUserAction;
+use App\Domains\Authentication\Http\Requests\EmailVerificationRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ForgotPasswordRequest;
 use App\Http\Requests\LoginRequest;
@@ -12,6 +13,7 @@ use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Events\Verified;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -21,7 +23,31 @@ use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function register(RegisterRequest $request, CreateUserAction $createUserAction)
+    /**
+     * @OA\Post(
+     *      path="/api/auth/register",
+     *      tags={"Authentication"},
+     *      summary="Register new user and gain access token.",
+     *      operationId="register",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Pass user details",
+     *          @OA\JsonContent(
+     *              required={"username", "email","password", "password_confirmation"},
+     *              @OA\Property(property="name", type="string", format="text", example="SomeFancyUsername"),
+     *              @OA\Property(property="email", type="string", format="email", example="vladimir.vulovic@quantox.com"),
+     *              @OA\Property(property="password", type="string", format="password", example="password"),
+     *              @OA\Property(property="password_confirmation", type="string", format="password", example="password"),
+     *          ),
+     *      ),
+     *      @OA\Response(response=200, description="Success", @OA\JsonContent()),
+     *      @OA\Response(response=422, description="Parameter(s) missing", @OA\JsonContent()),
+     * )
+     * @param RegisterRequest $request
+     * @param CreateUserAction $createUserAction
+     * @return JsonResponse
+     */
+    public function register(RegisterRequest $request, CreateUserAction $createUserAction): JsonResponse
     {
         $user = $createUserAction->execute($request->validated());
 
@@ -33,7 +59,29 @@ class AuthController extends Controller
         ], 201);
     }
 
-    public function login(LoginRequest $request)
+    /**
+     * @OA\Post(
+     *      path="/api/auth/login",
+     *      tags={"Authentication"},
+     *      summary="Authenticate using user credentials to gain access token.",
+     *      operationId="login",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Pass user credentials",
+     *          @OA\JsonContent(
+     *              required={"email","password"},
+     *              @OA\Property(property="email", type="string", example="member@quantox.com"),
+     *              @OA\Property(property="password", type="string", format="password", example="password"),
+     *          ),
+     *      ),
+     *      @OA\Response(response=200, description="Success", @OA\JsonContent()),
+     *      @OA\Response(response=400, description="Bad request", @OA\JsonContent()),
+     *      @OA\Response(response=422, description="Parameter(s) missing", @OA\JsonContent()),
+     * )
+     * @param LoginRequest $request
+     * @return JsonResponse
+     */
+    public function login(LoginRequest $request): JsonResponse
     {
         $user = User::where('email', $request->email)->first();
 
@@ -44,6 +92,19 @@ class AuthController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Post(
+     *      path="/api/auth/logout",
+     *      tags={"Authentication"},
+     *      summary="Logout user.",
+     *      operationId="logout",
+     *      @OA\Response(response=204, description="Success", @OA\JsonContent()),
+     *      @OA\Response(response=401, description="Unauthorized", @OA\JsonContent()),
+     *      security={
+     *          {"bearer_token": {}}
+     *      }
+     * )
+     */
     public function logout()
     {
         Auth::user()->tokens()->delete();
@@ -51,7 +112,26 @@ class AuthController extends Controller
         return response()->noContent();
     }
 
-    public function verifyEmail(Request $request)
+    /**
+     * @OA\Get(
+     *      path="/api/auth/email/verify/{id}/{hash}",
+     *      tags={"Authentication"},
+     *      summary="Verify account using signed URL received via email.",
+     *      operationId="verify",
+     *      @OA\Parameter(name="id", in="path", example="1"),
+     *      @OA\Parameter(name="hash", in="path", example="d48d03a1cfe1859b9cde06a6ecfdf17640bc930e"),
+     *      @OA\Parameter(name="expires", in="query", example="1633336613"),
+     *      @OA\Parameter(name="signature", in="query", example="7cf9ca062d99f230b557bd6c6d8db2ed2ee830a962423db5cead7e0e119a200c"),
+     *
+     *      @OA\Response(response=200, description="Success", @OA\JsonContent()),
+     *      @OA\Response(response=400, description="Bad request", @OA\JsonContent()),
+     *      @OA\Response(response=404, description="Not found", @OA\JsonContent()),
+     *      @OA\Response(response=401, description="Unauthorized", @OA\JsonContent()),
+     * )
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function verifyEmail(Request $request): JsonResponse
     {
         $user = User::find($request->route('id'));
 
@@ -70,6 +150,24 @@ class AuthController extends Controller
         ]);
     }
 
+    /**
+     * @OA\Post(
+     *      path="/api/auth/forgot-password",
+     *      tags={"Authentication"},
+     *      summary="Send forgot password email",
+     *      operationId="forgot-password",
+     *     @OA\RequestBody(
+     *          required=true,
+     *          description="Pass user email",
+     *          @OA\JsonContent(
+     *              required={"email"},
+     *              @OA\Property(property="email", type="string", example="member@quantox.com"),
+     *          ),
+     *      ),
+     *      @OA\Response(response=200, description="Success", @OA\JsonContent()),
+     *      @OA\Response(response=422, description="Parameter(s) missing", @OA\JsonContent()),
+     * )
+     */
     public function forgotPassword(ForgotPasswordRequest $request)
     {
         $status = Password::sendResetLink(
@@ -85,6 +183,26 @@ class AuthController extends Controller
         }
     }
 
+    /**
+     * @OA\Post(
+     *      path="/api/auth/reset-password",
+     *      tags={"Authentication"},
+     *      summary="Reset user password",
+     *      operationId="reset-password",
+     *     @OA\RequestBody(
+     *          required=true,
+     *          description="Pass user email, password and token from forgot password email",
+     *          @OA\JsonContent(
+     *              required={"email", "token", "password"},
+     *              @OA\Property(property="email", type="string", example="member@quantox.com"),
+     *              @OA\Property(property="password", type="string", format="password", example="password"),
+     *              @OA\Property(property="token", type="string", example="11c480d6bf0940803c6853cd6c7bb1f57371794500f53eae216721b19aa40864"),
+     *          ),
+     *      ),
+     *      @OA\Response(response=200, description="Success", @OA\JsonContent()),
+     *      @OA\Response(response=422, description="Parameter(s) missing", @OA\JsonContent()),
+     * )
+     */
     public function resetPassword(ResetPasswordRequest $request)
     {
         $status = Password::reset(
@@ -109,7 +227,22 @@ class AuthController extends Controller
         }
     }
 
-    public function resendVerificationEmail()
+    /**
+     * @OA\Post(
+     *      path="/api/auth/email/verification-notification",
+     *      tags={"Authentication"},
+     *      summary="Resend verification URL.",
+     *      operationId="resend",
+     *      @OA\Response(response=200, description="Success", @OA\JsonContent()),
+     *      @OA\Response(response=401, description="Unauthorized", @OA\JsonContent()),
+     *      security={
+     *          {"bearer_token": {}}
+     *      }
+     * )
+     *
+     * @return JsonResponse
+     */
+    public function resendVerificationEmail(): JsonResponse
     {
         Auth::user()->sendEmailVerificationNotification();
 
